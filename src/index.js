70c4a032c2d8b803bffd22f4cd771879ba046461
khronic79 <rustam.ptashinskiy@gmail.com>
2021-02-04 00:43:47 +0300

    init: make the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let sb = '';
+  let snew='';
+  while ((str!='') && (snew != str)) {
+    snew = str;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      sb = bracketsConfig[i][0]+bracketsConfig[i][1];
+      str = str.replace(sb,'');
+      sb = '';
+    }
+  }
+  if (str.length == '') {return true;}
+  else {return false;}
 }
